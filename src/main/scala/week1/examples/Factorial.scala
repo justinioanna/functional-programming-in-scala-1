38c03ef71529e711407d78baeb38677b0dd0ddd8
justin <justin.ioanna11@outlook.com>
2019-09-18 23:55:11 +0100

    Add tailrec annotation

--- src/main/scala/week1/examples/Factorial.scala
+++ src/main/scala/week1/examples/Factorial.scala
@@ -1,8 +1,11 @@
 package week1.examples
 
+import scala.annotation.tailrec
+
 object Factorial {
 
   def factorial(n: Int): Int = {
+    @tailrec
     def loop(acc: Int, n: Int): Int =
       if (n == 0) acc
       else loop(acc * n, n - 1)
