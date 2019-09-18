5dfce6981e3bfca427b293f95445308d223bf421
justin <justin.ioanna11@outlook.com>
2019-09-18 23:29:10 +0100

    Add Newton's method example

---
+++ src/main/scala/week1/examples/NewtonsMethod.scala
@@ -0,0 +1,27 @@
+package week1.examples
+
+object NewtonsMethod {
+
+  def abs(x: Double): Double = if (x < 0) -x else x
+
+  def sqrt(x: Double): Double = {
+
+    def sqrtIter(guess: Double): Double =
+      if (isGoodEnough(guess)) guess else sqrtIter(improve(guess))
+
+    def isGoodEnough(guess: Double): Boolean =
+      abs(guess * guess - x) / x < 0.001
+
+    def improve(guess: Double) =
+      (guess + x / guess) / 2
+
+    sqrtIter(1.0)
+  }
+
+  def main(args: Array[String]) {
+    println(sqrt(2))
+    println(sqrt(4))
+    println(sqrt(1e-6))
+    println(sqrt(1e60))
+  }
+}
