ee921f43c85207f05d93b4207a7ed5ea21d74a6d
justin <justin.ioanna11@outlook.com>
2019-09-19 00:15:43 +0100

    Add Pascal's triangle solution

---
+++ src/main/scala/week1/assignments/PascalsTriangle.scala
@@ -0,0 +1,17 @@
+package week1.assignments
+
+object PascalsTriangle {
+
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+}
