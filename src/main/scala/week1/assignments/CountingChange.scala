75493f08949e9343ddf910c8cb0b2cb5876a9ebe
justin <justin.ioanna11@outlook.com>
2019-09-19 22:52:11 +0100

    Refactor counting change

--- src/main/scala/week1/assignments/CountingChange.scala
+++ src/main/scala/week1/assignments/CountingChange.scala
@@ -4,8 +4,7 @@ object CountingChange {
 
   def countChange(money: Int, coins: List[Int]): Int =
     if (money == 0) 1
-    else if (money < 0) 0
-    else if (coins.isEmpty && money >= 1) 0
+    else if (money < 0 || coins.isEmpty) 0
     else countChange(money - coins.head, coins) + countChange(money, coins.tail)
 
 }
