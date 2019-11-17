a57b05103a6daec4f33543ec25199465e740e8cd
Justin Ioanna <justin.ioanna11@outlook.com>
2019-11-17 19:28:57 +0000

    Add IntSet exercise

--- src/main/scala/week3/exercises/IntSet.scala
+++ src/main/scala/week3/exercises/IntSet.scala
@@ -1,22 +1,16 @@
 package week3.exercises
 
-object intsets extends App {
-  val t1 = new NonEmpty(3, new Empty, new Empty)
-  val t2 = t1.incl(4).incl(7).incl(6)
-
-  println(t2)
-}
-
 abstract class IntSet {
   def contains(x: Int): Boolean
   def incl(x: Int): IntSet
+  def union(other: IntSet): IntSet
 }
 
-class Empty extends IntSet {
+object Empty extends IntSet {
   def contains(x: Int): Boolean = false
-  def incl(x: Int): IntSet = new NonEmpty(x, new Empty, new Empty)
-
+  def incl(x: Int): IntSet = new NonEmpty(x, Empty, Empty)
   override def toString = "."
+  def union(other: IntSet) = other
 }
 
 class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
@@ -28,6 +22,7 @@ class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
     if (x < elem) new NonEmpty(elem, left.incl(x), right)
     else if (x > elem) new NonEmpty(elem, left, right.incl(x))
     else this
-
   override def toString = "{" + left + elem + right + "}"
+  def union(other: IntSet): IntSet =
+    ((left union right) union other) incl elem
 }
