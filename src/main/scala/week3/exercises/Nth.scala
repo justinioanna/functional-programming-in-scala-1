b8686f166074fa24f47e6968c03310e5b4e761ee
Justin Ioanna <justin.ioanna11@outlook.com>
2019-11-17 19:42:12 +0000

    Add Nth exercise

---
+++ src/main/scala/week3/exercises/Nth.scala
@@ -0,0 +1,8 @@
+package week3.exercises
+
+object Nth {
+  def nth[T](n: Int, xs: List[T]): T =
+    if (xs.isEmpty) throw new IndexOutOfBoundsException
+    else if (n == 0) xs.head
+    else nth(n - 1, xs.tail)
+}
---
+++ src/test/scala/week3/exercises/NthSuite.scala
@@ -0,0 +1,34 @@
+package week3.exercises
+
+import Nth.nth
+import org.scalatest.FunSuite
+
+class NthSuite extends FunSuite {
+
+  val list = new Cons(1, new Cons(2, new Cons(3, new Nil)))
+
+  test("Start of list.") {
+    assert(nth(0, list) === 1)
+  }
+
+  test("Middle of list.") {
+    assert(nth(1, list) === 2)
+  }
+
+  test("End of list.") {
+    assert(nth(2, list) === 3)
+  }
+
+  test("Index out of range.") {
+    intercept[IndexOutOfBoundsException] {
+      nth(5, list)
+    }
+  }
+
+  test("Index negative.") {
+    intercept[IndexOutOfBoundsException] {
+      nth(-1, list)
+    }
+  }
+
+}
