d3ac0096223a97050d651cb7330b4e76a066714b
Justin Ioanna <justin.ioanna11@outlook.com>
2019-11-25 17:15:03 +0000

    Update variance of ConList

--- src/main/scala/week3/exercises/ConsList.scala
+++ src/main/scala/week3/exercises/ConsList.scala
@@ -1,21 +1,26 @@
 package week3.exercises
 
-trait List[T] {
+trait List[+T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
+  def prepend[U >: T](elem: U): List[U] = new Cons(elem, this)
 }
 class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty = false
 }
-class Nil[T] extends List[T] {
+object Nil extends List[Nothing] {
   def isEmpty: Boolean = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
 object List {
-  def apply[T]() = new Nil[T]
-  def apply[T](x1: T): List[T] = new Cons(x1, new Nil[T])
-  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil[T]))
+  def apply[T]() = Nil
+  def apply[T](x1: T): List[T] = new Cons(x1, Nil)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, Nil))
+}
+
+object test {
+  val x: List[String] = Nil
 }
