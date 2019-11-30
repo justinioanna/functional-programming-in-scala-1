7be1d280407b35090bdb70ec6443f129b4d9d465
Justin Ioanna <justin.ioanna11@outlook.com>
2019-11-30 21:18:21 +0000

    Generalise using ordering

--- src/main/scala/week5/exercises/MergeSort.scala
+++ src/main/scala/week5/exercises/MergeSort.scala
@@ -1,18 +1,20 @@
 package week5.exercises
 
+import math.Ordering
+
 object MergeSort {
-  def msort(xs: List[Int]): List[Int] = {
+  def msort[T](xs: List[T])(ord: Ordering[T]): List[T] = {
     val n = xs.length / 2
     if (n == 0) xs
     else {
-      def merge(xs: List[Int], ys: List[Int]): List[Int] = (xs, ys) match {
+      def merge(xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
         case (Nil, ys) => ys
         case (xs, Nil) => xs
         case (x :: xs1, y :: ys1) =>
-          if (x < y) x :: merge(xs1, ys) else y :: merge(xs, ys1)
+          if (ord.lt(x, y)) x :: merge(xs1, ys) else y :: merge(xs, ys1)
       }
       val (fst, snd) = xs.splitAt(n)
-      merge(msort(fst), msort(snd))
+      merge(msort(fst)(ord), msort(snd)(ord))
     }
   }
 }
--- src/test/scala/week5/exercises/MergeSortSuite.scala
+++ src/test/scala/week5/exercises/MergeSortSuite.scala
@@ -1,14 +1,28 @@
 package week5.exercises
 
-import MergeSort.msort
+import math.Ordering
 import org.scalatest.FunSuite
+
+import MergeSort.msort
 
 class MergeSortSuite extends FunSuite {
 
   val nums = List(2, -4, 7, 5, 1)
+  val fruits = List("apple", "pineapple", "orange", "banana")
 
-  test("Mergesort list.") {
-    assert(msort(nums) == List(-4, 1, 2, 5, 7))
+  test("Mergesort list of integers.") {
+    assert(msort(nums)(Ordering.Int) == List(-4, 1, 2, 5, 7))
+  }
+
+  test("Mergesort list of strings.") {
+    assert(
+      msort(fruits)(Ordering.String) == List(
+        "apple",
+        "banana",
+        "orange",
+        "pineapple"
+      )
+    )
   }
 
 }
